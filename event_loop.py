801d06d42587e86ff8f49d69d4fc53ed26ca0c8c
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-11-01 21:49:17 -0700

    Refactor player code, remove redundant event loop file

--- event_loop.py
+++ event_loop.py
@@ -25,26 +25,3 @@ class EventLoop:
                     self.action_map[event.type](event)    # execute events from map
                 except TypeError:
                     self.action_map[event.type]()       # event function may not accept any parameters
-    @staticmethod
-    def check_events(player):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    player.moving_right = True
-                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    player.moving_down = True
-                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    player.moving_left = True
-                elif event.key == pygame.K_w or event.key == pygame.K_UP: # not sure how to make him jump
-                    player.moving_up = True
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    player.moving_right = False
-                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    player.moving_down = False
-                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    player.moving_left = False
-                elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                    player.moving_up = False
